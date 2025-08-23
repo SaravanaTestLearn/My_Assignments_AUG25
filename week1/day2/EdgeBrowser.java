132e3004d9d36d6167672c12d9e57a9aa024f9a3
SaravanaTestLearn <56007805+SaravanaTestLearn@users.noreply.github.com>
2025-08-23 12:19:57 +0530

    Week-01_Day-01 and Day-02 Assignment_Saravana

---
+++ week1/day2/EdgeBrowser.java
@@ -0,0 +1,12 @@
+package week1.day2;
+
+public class EdgeBrowser {
+
+	public static void main(String[] args) {
+		Browser myBrowser = new Browser();
+		myBrowser.launchBrowser("Edge");
+		myBrowser.loadUrl();
+
+	}
+
+}
